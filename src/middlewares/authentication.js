e0cc0f4ecbf71b512561a3751bb1e5ead340985a
Leticia Missias <leticia.m.icm@gmail.com>
2022-07-19 17:55:35 -0300

    refatora a lógica do middleware de autenticação

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -5,13 +5,21 @@ const authentication = (req, res, next)=> {
     if(!req.headers.authorization) throw createErrorObj(400, '"token" is required');
     try{
       const payload = decodeToken(req.headers.authorization)
+      const {clientId: clientIdParam } = req.params;
       const { clientId } = req.body;
-      const { client_id: clientToken } = payload;
-      if(clientId !== clientToken) return res.status(400).json({message: "Action not allowed!"})
+      const { client_id: clientIdToken } = payload;
+
+      if(clientId !== clientIdToken && req.method === "POST"){
+       return res.status(400).json({ message: "Action not allowed!" })
+      };
+      if(Number(clientIdParam) !== clientIdToken && req.method === "GET"){
+       return res.status(400).json({ message: "Action not allowed!" })
+      };
+
       req.user = payload;
       next();
     } catch(error){
-      throw createErrorObj(400, "Expired or invalid token")
+      throw createErrorObj(400, error.message)
     }
 };
 
