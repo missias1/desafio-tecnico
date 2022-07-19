38c0eff09c40d906b980cc97f885cc09c8bec0b2
Leticia Missias <leticia.m.icm@gmail.com>
2022-07-19 17:54:28 -0300

    feat: adiciona regras de negocios no Service do endpoint /conta/saque

--- src/services/serviceConta.js
+++ src/services/serviceConta.js
@@ -1,15 +1,24 @@
 const modelConta = require('../models/modelConta');
 const createErrorObj = require('../utils/createErrorObj')
 
-const addCashInWallet = async (value, clientId)=> {
-  const [addCash] = await modelConta.addCashInWallet(value, clientId);
+const increaseCashInWallet = async (value, clientId)=> {
+  const [addCash] = await modelConta.increaseCashInWallet(value, clientId);
   
   if(addCash.affectedRows === 0) throw createErrorObj(400, "Deposit failed");
 };
 
-const removeCashFromWallet = async (value, clientId)=> {
-  const [removeCash] = await modelConta.removeCashFromWallet(value, clientId);
+const decreaseCashFromWallet = async (value, clientId)=> {
+  //Acessa saldo do cliente
+  const [walletInfo] = await modelConta.getWalletInfoById(clientId);
+  const { balance } = walletInfo;
 
+  //verifica se o valor do saque é maior que o disponível em conta
+  if(value > balance) throw createErrorObj(400, "You do not have this value in your account!")
+
+  //realiza o saque da conta
+  const [removeCash] = await modelConta.decreaseCashFromWallet(value, clientId);
+
+  //verifica se o saque não ocorreu
   if(removeCash.affectedRows === 0) throw createErrorObj(400, "Withdraw failed!");
 };
 
@@ -21,7 +30,7 @@ const getWalletInfoById = async (clientId)=> {
 };
 
 module.exports = {
-  addCashInWallet,
-  removeCashFromWallet,
+  increaseCashInWallet,
+  decreaseCashFromWallet,
   getWalletInfoById,
 }
