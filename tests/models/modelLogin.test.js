29ced2ac26a36ab30a7232f0ed5bc6c62b8c0200
Leticia Missias <leticia.m.icm@gmail.com>
2022-07-21 20:58:07 -0300

    test: adiciona testes na camada Model da rota Login

--- tests/models/modelLogin.test.js
+++ tests/models/modelLogin.test.js
@@ -1,2 +1,93 @@
 const sinon = require('sinon');
 const {expect} = require('chai');
+const connection = require('../../src/database/connection');
+const modelLogin = require('../../src/database/models/modelLogin');
+const mocks = require('./mocks');
+
+describe('Procura pelo cliente no banco de dados',()=>{
+
+  describe('Usuário é encontrado com sucesso', ()=> {
+
+    beforeEach(()=>{
+      sinon.stub(connection, 'execute').resolves([mocks.USER_INFO]);
+    });
+    afterEach(()=>{
+      connection.execute.restore();
+    });
+
+    const { email, password } = mocks.USER_INFO
+    it('Verifica se retorna um objeto', async ()=> {
+      const user = await modelLogin.findClient(email, password);
+      expect(user).to.be.an('object');
+    })
+    it('Verifica se retorna as chaves esperadas', async ()=> {
+      const user = await modelLogin.findClient(email, password);
+      expect(user).to.include.all.keys('email', 'clientId');
+    })
+
+  });
+
+  describe('Usuário não é encontrado', ()=> {
+
+    beforeEach(()=>{
+      sinon.stub(connection, 'execute').resolves([undefined]);
+    });
+    afterEach(()=>{
+      connection.execute.restore();
+    });
+
+    const data = {
+      email:"desconhecido@gmail.com",
+      password:"123456"
+    }
+
+    it('Verifica se retorna undefined', async ()=> {
+      const user = await modelLogin.findClient(data.email, data.password);
+      expect(user).to.be.undefined;
+    });
+
+  });
+
+});
+
+describe('Procura pelo cliente no banco de dados por meio do id',()=>{
+
+  describe('Usuário é encontrado com sucesso', ()=> {
+
+    beforeEach(()=>{
+      sinon.stub(connection, 'execute').resolves([mocks.USER_INFO]);
+    });
+    afterEach(()=>{
+      connection.execute.restore();
+    });
+
+    const { clientId } = mocks.USER_INFO
+    it('Verifica se retorna um objeto', async ()=> {
+      const user = await modelLogin.findClientById(clientId);
+      expect(user).to.be.an('object');
+    })
+    it('Verifica se retorna as chaves esperadas', async ()=> {
+      const user = await modelLogin.findClient(clientId);
+      expect(user).to.include.all.keys('email', 'clientId');
+    })
+
+  });
+
+  describe('Usuário não é encontrado', ()=> {
+
+    beforeEach(()=>{
+      sinon.stub(connection, 'execute').resolves([undefined]);
+    });
+    afterEach(()=>{
+      connection.execute.restore();
+    });
+
+    const clientId = 100;
+
+    it('Retorna undefined', async ()=> {
+      const user = await modelLogin.findClientById(clientId)
+    });
+
+  });
+
+});
