928db72326da180ca1f6f4938ab51327250edd4f
Leticia Missias <leticia.m.icm@gmail.com>
2022-07-24 00:51:17 -0300

    refatora lógica do serviceRegister

--- src/database/models/modelLogin.js
+++ src/database/models/modelLogin.js
@@ -13,9 +13,9 @@ const findClientById = async (id)=> {
   );
   return client;
 };
-
+// fazer teste dele
 const findClientByEmail = async (email)=> {
-  const client = await connection.execute(
+  const [[client]] = await connection.execute(
     `SELECT email FROM heroku_3a2342a6c76f266.clients WHERE email = ?;`, [email])
   return client;
 }
--- src/services/serviceRegister.js
+++ src/services/serviceRegister.js
@@ -4,9 +4,9 @@ const createErrorObj = require('../utils/createErrorObj');
 
 const addClient = async ({ name, email, password, telephone })=> {
 
-  const clientEmail = await modelLogin.findClientByEmail(email);
+  const findEmail = await modelLogin.findClientByEmail(email);
 
-  if(clientEmail === email ) throw createErrorObj(400, "Email already exist!");
+  if(findEmail) throw createErrorObj(400, "Email already exist!");
 
   const [result] = await modelRegister.addClient(name, email, password, telephone)
 
